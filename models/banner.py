05b4754ea42106030508bb84b72b20f8d23304bf
Jonathan Lieu <razerlieu@gmail.com>
2021-10-20 20:54:15 -0700

    Create banner.py

---
+++ models/banner.py
@@ -0,0 +1,6 @@
+class Banner:
+  def __init__(self, id: int, discord_id: int, times_picked: int):
+    self.id = id
+    self.requested_time = requested_time
+    self.discord_id = discord_id
+    self.times_picked = times_picked
