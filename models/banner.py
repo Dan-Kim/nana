55ede50d92506a83d1e9074ae75f7c611aeaff10
Jonathan Lieu <jonathan.lieu.57@my.csun.edu>
2021-10-25 13:34:00 -0700

    Update banner.py
    removed id field in Banner

--- models/banner.py
+++ models/banner.py
@@ -1,5 +1,4 @@
 class Banner:
-  def __init__(self, id: int, discord_id: int, times_picked: int):
-    self.id = id
+  def __init__(self, discord_id: int, times_picked: int):
     self.discord_id = discord_id
     self.times_picked = times_picked
