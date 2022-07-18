91fbabb2dc33e650e9fb30359cdff252d2fe963b
dan-kim <dkim478@berkeley.edu>
2022-07-18 01:57:17 -0700

    fix

--- cogs/myanimelist.py
+++ cogs/myanimelist.py
@@ -15,7 +15,8 @@ class MyAnimeList(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
     self.sched = AsyncIOScheduler(daemon=True)
-    self.sched.add_job(func=self.poll_rss_feeds, trigger='cron', args=[], max_instances=1, minute='5')
+    self.sched.add_job(func=self.poll_rss_feeds, trigger='cron', args=[], max_instances=1, minute='0,5,10,15,20,25,'
+                                                                                                  '30,35,40,45,50,55')
     self.sched.start()
 
   async def poll_rss_feeds(self):
@@ -27,7 +28,7 @@ class MyAnimeList(commands.Cog):
         for item in xml.findall('./channel/item'):
           media_title, link, description, pub_date = item[0], item[1], item[3], item[4]
           pub_timestamp = datetime.strptime(pub_date.text, '%a, %d %b %Y %H:%M:%S %z').timestamp()
-          if pub_timestamp > (datetime.now() - timedelta(minutes=5)).timestamp():
+          if pub_timestamp > (datetime.now() - timedelta(minutes=10)).timestamp():
             await channel.send(embed=make_rss_feed_update_embed(media_title=media_title.text, link=link.text,
                                                                 description=description.text, pub_date=pub_date.text,
                                                                 media_type=media_type, user=user))
